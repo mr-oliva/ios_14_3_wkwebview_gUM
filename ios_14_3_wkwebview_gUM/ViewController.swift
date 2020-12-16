5c192742953e89ca4e0bca2d70ecd5dd300019c9
mr-oliva <kutsuzawa2851@gmail.com>
2020-12-16 21:42:29 +0900

    Initial Commit

---
+++ ios_14_3_wkwebview_gUM/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ios_14_3_wkwebview_gUM
+//
+//  Created by mr-oliva on 2020/12/16.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
